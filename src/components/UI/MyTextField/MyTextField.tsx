94c26627554ad7a00d8b9e20fc921f03ea4e9598
shoegaev <kshoegaev@gmail.com>
2024-12-07 21:35:47 +0300

    feat: create myTextField component

---
+++ src/components/UI/MyTextField/MyTextField.tsx
@@ -0,0 +1,66 @@
+import React, {FormEventHandler} from "react";
+import cl from "./MyTextField.module.scss";
+
+// Default value - hidden
+type scrollProperty = "Hidden" | "Auto" | "Visible" | "Scroll";
+
+interface MyTextFieldProps {
+  controllParams: [string, React.Dispatch<React.SetStateAction<string>>];
+  cssClasses?: string[];
+  inputMode?: "text" | "decimal" | "numeric";
+  symbolsRestrictions?: RegExp;
+  onInput?: FormEventHandler<HTMLTextAreaElement>;
+  wrap?: "off" | "hard";
+  overflow?: {
+    x: scrollProperty;
+    y: scrollProperty;
+  };
+}
+
+const MyTextField = ({
+  cssClasses,
+  controllParams,
+  symbolsRestrictions,
+  onInput,
+  wrap = "off",
+  overflow = {x: "Hidden", y: "Hidden"},
+  ...props
+}: MyTextFieldProps) => {
+  const [value, setValue] = controllParams;
+  const classNameArr = [cl.TextField, ...(cssClasses ?? [])];
+  if (overflow) {
+    classNameArr.push(
+      cl[`textArea_overflowY${overflow.y}`],
+      cl[`textArea_overflowX${overflow.x}`],
+    );
+  }
+
+  return (
+    <div className={classNameArr.join(" ")}>
+      <textarea
+        wrap={wrap}
+        className={cl.TextField__textArea}
+        value={value}
+        onInput={e => {
+          const nativeE = e.nativeEvent;
+          if (onInput) onInput(e);
+          const currValue = e.currentTarget.value;
+
+          if (
+            !symbolsRestrictions ||
+            (nativeE instanceof InputEvent &&
+              (nativeE.data?.match(symbolsRestrictions) ||
+                nativeE.data === null))
+          ) {
+            setValue(currValue);
+          } else {
+            return;
+          }
+        }}
+        {...props}
+      />
+    </div>
+  );
+};
+
+export default MyTextField;
